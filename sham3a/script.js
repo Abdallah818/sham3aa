cb9072f6cf9819b453793e13ddd46ffdf3a647ad
Abdallah818 <bb2432009@gmail.com>
2025-02-02 03:38:33 +0200

    Delete sham3a directory

--- sham3a/script.js
+++
@@ -1,8 +0,0 @@
-// تأثيرات الشمعة المتحركة
-document.addEventListener("DOMContentLoaded", function () {
-    const flame = document.querySelector('.flame');
-    
-    setInterval(() => {
-        flame.style.animationDuration = `${Math.random() * 1 + 0.5}s`; // تغيير سرعة اللهب
-    }, 100);
-});
